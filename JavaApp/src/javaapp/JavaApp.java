be3345970dd02da47755d5f5a59d4db7584c8e78
Gokulakrishnan 54 AIML A <231501054@rajalakshmi.edu.in>
2024-11-19 15:06:24 +0530

    Add files via upload

---
+++ JavaApp/src/javaapp/JavaApp.java
@@ -0,0 +1,18 @@
+
+package javaapp;
+
+
+public class JavaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login loginframe=new login();
+        loginframe.setVisible(true);
+        loginframe.pack();
+        loginframe.setLocationRelativeTo(null);
+    }
+    
+}
